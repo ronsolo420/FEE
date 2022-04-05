d4d4bd072d6435da977df44cbb95d096d6a3d6d2
ronsolo420 <34901823+ronsolo420@users.noreply.github.com>
2022-04-05 06:06:51 -0700

    1

--- javascript/game.js
+++ javascript/game.js
@@ -1,23 +1,49 @@
 let Game = {
-    serGameStart: function(classType) {
+    setGameStart: function(classType) {
         this.resetPlayer(classType);
         this.setPreFight();
     },
     resetPlayer: function(classType) {
         switch (classType) {
-        case "Warrior"://! health, mana, strength, agility, speed
+        case "Alucard"://! health, mana, strength, agility, speed
             player = new Player (classType, 200, 0, 200, 100, 50);
             break;
-        case "Rogue":
+        case "Richter":
             player = new Player (classType, 100, 0, 100, 150, 200); 
             break; 
-        case "Mage":
+        case "Maria":
             player = new Player (classType, 80, 0, 50, 200, 50);
             break;      
         }
+        let getInterface = document.querySelector(".interface");
+        getInterface.innerHTML = '<img src="../images/img/avatar-player/' + classType.toLowerCase() + '.jpg" class="img-avatar"><div><h3>' + classType + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strength: ' + player.strength + '</p><p>Agility' + player.agility+ '</p><p>Speed: ' + player.speed + '</p></div';        
     },
-    setPreFight: function(){
-
+    setPreFight: function() {
+       let getHeader = document.querySelector(".header"); 
+       let getActions = document.querySelector(".actions"); 
+       let getArena = document.querySelector(".arena"); 
+       getHeader.innerHTML = '<p>Task: Find an enemy!'
+       getActions.innerHTML = '<a href="#" class="btn=PreFight" onclick="Game.setFight()">Search for enemy!</a>';
+       getArena.style.visibility = "visible";
     },
-
-}
+    setFight: function() {
+        let getHeader = document.querySelector(".header"); 
+        let getActions = document.querySelector(".actions"); 
+        let getEnemy = document.querySelector(".enemy");  
+        // Create enemy!
+        let enemy00 = new Enemy("Goblin",100,0,50,100,100);
+        let enemy01 = new Enemy("Skeleton",200,0,150,80,150);
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+        switch (chooseRandomEnemy) {
+            case 0:
+                enemy = enemy00;   
+            break;
+            case 1:
+                enemy = enemy01;
+            break;    
+            }
+        getHeader().innerHTML = '<p>Task: Choose your move!</p>';
+        getActions.innerHTML = '<a href="#" class="btn=PreFight"onclick="PlayerMoves.calcAttack()">Attack!</a>'; getEnemy.innerHTML = '<img src="../images/img/avatar-enemies/' + 
+        enemy.enemyType.toLowerCase() + '.png" alt="' + enemy.enemyType + ' " class="img-avatar"><div><h3>' + enemy.enemyType + '<h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+    }
+}    
--- javascript/player.js
+++ javascript/player.js
@@ -8,4 +8,68 @@ function Player(classType, health, mana, strength, agility, speed,) {
     this.agility = agility;
     this.speed = speed;
 }
+let PlayerMoves= {
+    calcAttack: function(){
+        //Who attacks first?
+        let getPlayerSpeed = player.speed;
+        let getEnemySpeed = enemy.speed;
+    //Player attacks!
+    let playerAttack = function(){
+        let calcBaseDamage;
+        if (player.mana > 0) {
+            calcBaseDamage = player.strength * player.mana /1000;
+        } else {
+            calcBaseDamage = player.strength * player.agility /1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage + offsetDamage;
+        //number of hits that
+        let numberOfHits = Math.floor(Math.random() * Math.floor( player.agility /10) /2) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    }
+    //Enemy attacks!
+    let enemyAttack = function(){
+        let calcBaseDamage;
+        if (enemy.mana > 0) {
+            calcBaseDamage = enemy.strength * enemy.mana /1000;
+        } else {
+            calcBaseDamage = enemy.strength * enemy.agility /1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage + offsetDamage;
+        //number of hits that
+        let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility /10) /2) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    } 
+    let getPlayerHealth = document.querySelector(".health-player");
+    let getEnemyHealth = document.querySelector(".health-enemy");
 
+    if (getPlayerSpeed >= getEnemySpeed) {
+        let playerAttackValues = playerAttack();
+        let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+        enemy.health = enemy.health - totalDamage;
+        alert("You hit" + playerAttackValues[0] + "damage" + playerAttackValues[1] + "times.");
+        if (enemy.health<= 0) {
+            alert("You win! Refresh browser to play again.");
+            getPlayerHealth.innerHTML = 'Health: ' + player.health;
+            getEnemyHealth.innerHTML = 'Health: 0';
+        } else {
+            getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        let enemyAttackValues = enemyAttack ();
+        let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+        player.health = player.health - totalDamage;
+        alert("You hit" + enemyAttackValues[0] + "damage" + enemyAttackValues[1] + "times.");
+        if (player.health<= 0) {
+            alert("You loose! Refresh browser to play again.");
+            getPlayerHealth.innerHTML = 'Health: 0';
+            getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        } else {
+            getEnemyHealth.innerHTML = 'Health: ' + player.health; 
+        }
+    }
+    }
+    }
+
+}
