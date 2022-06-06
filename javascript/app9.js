107e2bdd860519051e0e04f44d65ed395e45a231
ronsolo420 <34901823+ronsolo420@users.noreply.github.com>
2022-06-06 11:18:51 -0700

    1

---
+++ javascript/app9.js
@@ -0,0 +1,20 @@
+function mapForEach (arr, fn) {
+
+    var newArr = [];
+    for (var i=0; i < arr.length; i++) {
+        newArr.push(
+        fn(arr[i])
+    )
+    };
+
+    return newArr;
+}
+
+var arr1 = [1,2,3];
+console.log(arr1);
+
+var arr2 = mapForEach (arr1, function(item) {
+    return item * 4;
+});
+
+console.log(arr2);
