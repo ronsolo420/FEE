13e71346b01a274240d62e34ac3b44d40731bc4f
ronsolo420 <34901823+ronsolo420@users.noreply.github.com>
2022-06-24 12:13:00 -0700

    1

---
+++ reactFolder/src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+    {
+      name: "Trey Lance",
+      imgURL:
+        "https://express-images.franklymedia.com/6616/sites/82/2022/06/17130112/TreyLance124124.png",
+      phone: "+123 456 789",
+      email: "t@lance.com"
+    },
+    {
+      name: "Deebo Samuel",
+      imgURL:
+        "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fninernoise.com%2Fwp-content%2Fuploads%2Fimagn-images%2F2021%2F09%2F16740661.jpeg",
+      phone: "+987 654 321",
+      email: "d@samuel.com"
+    },
+    {
+      name: "Brandon Aiyuk",
+      imgURL:
+        "https://images.spot.im/v1/production/jeggc8qd9gy0dozfy8rc",
+      phone: "+918 372 574",
+      email: "b@aiyuk.com"
+    }
+  ];
+  
+  export default contacts;
+  
